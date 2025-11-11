bf3d39948a2c55991d5053b41e3d768f502a5eec
S.Krishnan <skrish22195@gmail.com>
2025-11-11 21:53:51 +0530

    change grid background

--- src/review/components/ReviewApp.tsx
+++ src/review/components/ReviewApp.tsx
@@ -14,9 +14,9 @@ export const ReviewApp = () => {
   };
 
   return (
-    <div className="h-screen bg-white relative overflow-hidden flex flex-col">
+    <div className="min-h-screen relative flex flex-col items-center px-4 py-10">
       {/* Main Content */}
-      <div className="relative px-6 py-6 flex-1 flex flex-col">
+      <div className="relative w-full max-w-6xl flex-1 flex flex-col gap-6 px-4 sm:px-6">
         {/* Header */}
         <div className="max-w-6xl mx-auto mb-6 w-full">
           <div className="flex items-center justify-between">
