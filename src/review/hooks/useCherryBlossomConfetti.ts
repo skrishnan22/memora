93dee9587404f9138c6f15db20565b9edeee3378
S.Krishnan <skrish22195@gmail.com>
2025-11-16 17:56:21 +0530

    iterate on confetti sizes

--- src/review/hooks/useCherryBlossomConfetti.ts
+++ src/review/hooks/useCherryBlossomConfetti.ts
@@ -32,8 +32,15 @@ export const useCherryBlossomConfetti = () => {
     const viewportWidth = window.innerWidth || 0;
     const viewportHeight = window.innerHeight || 0;
     const columnWidth = viewportWidth / (DEFAULT_COLUMNS + 1);
+    // start at a bit over the top of the viewport to have the confetti drift from top instead of middle
     const dispatchY = Math.max(-120, viewportHeight * -0.1);
 
+    /**
+     * Using a wave + split column approach to create a more natural looking confetti effect.
+     * js-confetti only supports burst from a point, so to create a blanket draw effect, this is workaround.
+     * Columns handle width coverage, waves handle time. Triggering all columns simultaneously makes
+     * one dense burst that’s over in a second
+     */
     Array.from({ length: DEFAULT_WAVES }).forEach((_, waveIndex) => {
       Array.from({ length: DEFAULT_COLUMNS }).forEach((_, columnIndex) => {
         const triggerDelay =
@@ -42,8 +49,8 @@ export const useCherryBlossomConfetti = () => {
         window.setTimeout(() => {
           confettiRef.current?.addConfettiAtPosition({
             emojis: BLOSSOM_EMOJIS,
-            emojiSize: 80,
-            confettiNumber: 20,
+            emojiSize: 50,
+            confettiNumber: 10,
             confettiRadius: 8,
             confettiDispatchPosition: {
               x: columnWidth * (columnIndex + 1),
